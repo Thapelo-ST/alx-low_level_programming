6953146117603a67fbad6468cb6e27c4124583a1
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-17 15:42:21 -0800

    what?

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,30 @@
 
 int main(void)
 {
-	int n = 0;
+	int i = 0, j;
 
-	while (n < 9)
+	while (i < 10)
 	{
-		putchar(n + '0');
-		putchar(',', ' ');
-		n++;
+		j = i + 1;
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
 	}
-	putchar('9');
-	putchar('\n');
+
 	return (0);
 }
