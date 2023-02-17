f389d01615ec62b07b6802ddb442983d93d0a2c1
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-17 15:04:35 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry stream
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ahlpa = 'z';
+
+	while (ahlpa >= a)
+	{
+		putchar(ahlpa);
+		ahlpa--;
+	}
+	putchar('\n');
+	return (0);
+}
