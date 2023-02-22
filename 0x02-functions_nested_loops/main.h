955f830b7089f78af0ac56672a99bd1a82dba127
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-21 17:51:44 -0800

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,52 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * _putchar - Writes a character to the standard output
+ *
+ * @c: The character to write
+ *
+ * Return: On success, the character written is returned. On error, -1 is
+ *         returned and errno is set appropriately.
+ */
+
+/*task 0*/
+int _putchar(int p)
+{
+	putchar(p);
+}
+/*task 1*/
+void print_alphabet(void)
+{
+	chr lett = 'a';
+
+	while (lett < 'z')
+	{
+		_putchar(lett);
+		lett++;
+	}
+}
+/*task 2*/
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char lett = 'a';
+	int count = 0;
+
+	while (count < 10)
+	{
+		while (lett < 'z')
+		{
+			_putchar(lett);
+			lett++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
+#endif /* MAIN_H */
