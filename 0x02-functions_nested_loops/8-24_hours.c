40d16cd1a969f5d06181c1a6dc5a8fc070d2ea4b
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-21 19:27:39 -0800

    23:30:57

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int hours, mins;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(':');
+			_putchar('0' + mins / 10);
+			_putchar('0' + mins % 10);
+			_putchar('\n');
+		}
+	}
+}
