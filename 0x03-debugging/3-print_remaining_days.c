2b55e693badd9b1c113af7ab679d896a5f098e2b
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-23 04:18:33 -0800

    Remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,40 +9,27 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int days_left = 0;
-	int i;
-
-	/* check for valid month */
-	if (month < 1 || month > 12)
+	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
-
-	/* check for valid day */
-	if (day < 1 || day > days_in_month[month - 1])
+	else
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-
-	/* check for leap year */
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-	{
-		days_in_month[1] = 29;
-	}
-
-	/* calculate days left in the year */
-	for (i = month - 1; i < 12; i++)
-	{
-		days_left += days_in_month[i];
-	}
-	days_left -= day;
-
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", days_left);
 }
