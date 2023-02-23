bc599a1d533296b630b2f9f8ea14026807ae5148
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-23 14:28:23 -0800

    Moonlight looks bettr in the scope

---
+++ 0x04-more_functions_nested_loops/8-print-square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints squareof size size
+ *
+ * @size: integer to set square size
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
