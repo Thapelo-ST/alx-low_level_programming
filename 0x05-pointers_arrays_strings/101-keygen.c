c811029141c665b3e340631a27816191c8bf996f
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-27 18:41:35 -0800

    Crack me if you can

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,16 @@
  */
 int main(void)
 {
-	int password[5]; // assume password is 5 digits long
+	int password[5];
 	int i;
 
-	srand(time(NULL)); // seed the random number generator with current time
+	srand(time(NULL));
 
-	// generate a random digit between 0 and 9 for each character in the password
 	for (i = 0; i < 5; i++) {
 		password[i] = rand() % 10;
 	}
 
-	// print the password
 	printf("%d%d%d%d%d\n", password[0], password[1], password[2], password[3], password[4]);
 
-	return 0;
+	return (0);
 }
