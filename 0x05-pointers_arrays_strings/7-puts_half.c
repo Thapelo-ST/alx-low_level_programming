5fe3f7a9a1afd73270cd02eeb1445bb404322c74
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-27 17:18:22 -0800

    Second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include "main.h"
 
+
+/**
+ * _strlen - checks string length
+ *
+ * @s: a pointer to a char
+ *
+ * Return: length of string.
+ *
+ * Description: This function checks how long is the string and returns the
+ *              length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
 /**
  * puts_half - Prints the second half of a string, followed by a new line.
  *
