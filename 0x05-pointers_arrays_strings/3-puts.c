ea391bd5719f192c88aa04c4ac5bc4bd4de9918a
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-27 16:25:21 -0800

    i fear computers, too much terminator and matrix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _puts - makes the string have a new line after it ran through the function
+ *
+ * @str: a pointer to an charecter
+ *
+ * Description: This function checks string pointed to by str and when its
+ *              done it puts a new line.
+ */
+
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		putchar(str);
+		str++;
+	}
+	putchar('\n');
+
+}
