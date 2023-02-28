06f41e740e207768d49aaad2698ecf4379729bc4
Thapelo-ST <tsamkelo96@gmail.com>
2023-02-27 17:57:20 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
 #include <limits.h>
 
+
 int _atoi(char *s)
 {
-	int sign = 1, r = 0, i = 0;
+	int sign = 1;
+	int num = 0;
+	int i = 0;
 
-	/*skiping white spaces*/
-	while (s[i] == ' ')
-		i++;
-
-	/*handle sign*/
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-
-	/*handle digits*/
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		/*check for overflow*/
-		if (r > INT_MAX / 10 || (r == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-		{
-			/*overflow detected*/
-			if (sign == 1)
-			{
-				return (INT_MAX);
-			}
-			else
-			{
-				return (INT_MIN);
-			}
+	while (s[i] != '\0') {
+		if (s[i] == '-') {
+			sign = -sign;
 		}
-
-		r = r * 10 + (s[i] - '0');
+		else if (s[i] == '+') {
+			sign = 1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9') {
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (num != 0) {
+			break;
+		}
 		i++;
 	}
-	return (sign * r);
+
+	return sign * num;
 }
