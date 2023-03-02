2c986c6b1e7fc6a1d5e8f8af1ab2d5070401aab3
Thapelo-ST <tsamkelo96@gmail.com>
2023-03-02 04:32:39 -0800

    its infinite

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers.
+ * @n1: First number
+ * @n2: Second number
+ * @r: result buffer
+ * @size_r: result buffer size
+ *
+ * Return: 0 if the result can not be stored in r,
+ * otherwise a pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, carry = 0, sum = 0;
+	char *p1 = n1, *p2 = n2, *pr = r + size_r - 1;
+
+	while (*p1++)
+		len1++;
+	while (*p2++)
+		len2++;
+
+	if (len1 + len2 + 1 > size_r)
+		return (0);
+
+	*pr = '\0';
+	p1 = n1 + len1 - 1;
+	p2 = n2 + len2 - 1;
+	while (p1 >= n1 || p2 >= n2 || carry)
+	{
+		sum = carry;
+		if (p1 >= n1)
+		{
+			sum += (*p1--) - '0';
+		}
+		if (p2 >= n2)
+		{
+			sum += (*p2--) - '0';
+		}
+		if (pr == r)
+		{
+			return (0);
+		}
+		carry = sum / 10;
+		*--pr = (sum % 10) + '0';
+	}
+
+	return (pr);
+}
