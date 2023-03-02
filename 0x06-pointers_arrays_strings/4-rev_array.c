1855235383e1740dab319c923cc674de8bbdd789
Thapelo-ST <tsamkelo96@gmail.com>
2023-03-01 23:49:53 -0800

    Reverse king

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,21 +3,22 @@
 /**
  * reverse_array - reverses the contents of an array
  * @a: integer array to be reversed
- * @n: @n: number of elements in array
+ * @n: number of elements in array
  *
  * Return: void
  */
 
 void reverse_array(int *a, int n)
 {
-	int i = n - 1;
-	int tmp;
+	int i, tmp;
 
+	n = n - 1;
+	i = 0;
 	while (i < n)
 	{
-		t = *(a + i);
+		tmp = *(a + i);
 		*(a + i) = *(a + n);
-		*(a + n) = t;
+		*(a + n) = tmp;
 		i++;
 		n--;
 	}
