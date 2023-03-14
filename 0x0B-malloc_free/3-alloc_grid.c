579312ac03560e82260a2fa887cf60be5072d9c8
Thapelo-ST <tsamkelo96@gmail.com>
2023-03-14 08:08:31 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,11 +21,11 @@ int **alloc_grid(int width, int height)
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
-		for (i = 0; i < height; i++)
+		for (i = 0; i <= height; i++)
 		{
 			free(grid[i]);
 		}
-		free(grid);
+		/*free(grid);*/
 		return (NULL);
 	}
 
