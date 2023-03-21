7abc9935bb7de70554e85d4677daca7bba910a1c
Thapelo-ST <tsamkelo96@gmail.com>
2023-03-21 16:10:51 -0700

    Main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define FILENAME __FILE__
+
+/**
+ * main = entry stream
+ *
+ * RETURN: Sucess 0
+ *
+ */
+
+int main()
+{
+	printf("The name of the current file is: %s\n", FILENAME);
+	return (0);
+}
