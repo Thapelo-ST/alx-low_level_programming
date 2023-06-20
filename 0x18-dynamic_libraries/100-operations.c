49684ff6983ce93ad71d17b99c7bea0e4db5a7ea
Thapelo-ST <tsamkelo96@gmail.com>
2023-06-20 16:32:57 -0700

    operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * add - ...
+ * Return: ...
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - ...
+ * Return: ...
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - ...
+ * Return: ...
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - ...
+ * Return: ...
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - ...
+ * Return: ...
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
