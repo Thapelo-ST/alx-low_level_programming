93e90a0bbe42cb35c680213382430452a8061b57
Thapelo-ST <tsamkelo96@gmail.com>
2023-10-10 04:39:18 +0200

    mandetory

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+/**
+ * binary_search - ...
+ * @array: ...
+ * @size: ...
+ * @value: ...
+ * Return: ...
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	/*int lft, mid;*/
+	size_t i, lft, mid, rgt;
+
+
+	lft = 0;
+	rgt = size - 1;
+	while (lft <= rgt)
+	{
+
+		mid = lft + (rgt - lft) / 2;
+		printf("Searching in array: ");
+		for (i = lft; i <= rgt; i++)
+		{
+			if (i == rgt)
+			{
+				printf("%d", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			lft = mid + 1;
+		}
+		else
+		{
+			rgt = mid - 1;
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,10 @@
+## C - Search algorithms
+
+A search algorithm is a step-by-step method or procedure used to locate specific data within a collection of data
+
+
+The ones on the mandetory tasks are 
+- Linear Search
+- Binary Search
+
+and the time complexity of other search algorithims
